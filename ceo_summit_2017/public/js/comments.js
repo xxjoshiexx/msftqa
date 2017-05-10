491693871c8c875becd37417eda41c8b7f27d679
tyler <scottt2@uw.edu>
2017-05-10 15:40:40 -0700

    Update comments.

--- ceo_summit_2017/public/js/comments.js
+++ ceo_summit_2017/public/js/comments.js
@@ -3,60 +3,60 @@ var comments = [
     avatarSrc: '../images/avatars/JoniS.jpg',
     name: 'Joni Sherman',
     comment: 'So proud to work for a company with such high standardsk and integrity!',
-    timeString: '00:02'
+    timeString: '00:22'
   },
   {
-    avatarSrc: '../images/avatars/BenW.jpg',
-    name: 'Ben Walters',
+    avatarSrc: '../images/avatars/EnricoC.jpg',
+    name: 'Enrico Cattaneo',
     comment: 'Hard to innovate when you don’t know if you will be asked to leave the US at any time.',
-    timeString: '00:08'
+    timeString: '00:28'
   },
   {
     avatarSrc: '../images/avatars/IrvinS.jpg',
     name: 'Irvin Sayers',
     comment: 'Thank you for take the time to address the Microsoft Community, for reiterating our values and acknowledging the many questions outstanding about what Microsoft will be doing to protect its H1-B and L1 visa employees.',
-    timeString: '00:15'
+    timeString: '00:35'
   },
   {
     avatarSrc: '../images/avatars/EmilyB.jpg',
     name: 'Emily Braun',
     comment: 'A constructive recommendation to help people in need.',
-    timeString: '00:20'
+    timeString: '00:40'
   },
   {
     avatarSrc: '../images/avatars/MeganB.jpg',
     name: 'Megan Bowen',
     comment: 'Do you see MSPAC priorities changing in response to the new political climate?',
-    timeString: '00:31'
+    timeString: '00:51'
   },
   {
     avatarSrc: '../images/avatars/JohannaL.jpg',
     name: 'Johanna Lorenz',
     comment: 'A big heart-filled thank you to the SLT for consistently doing everything you responsibly can to stand up against the divisive and unjust policies.',
-    timeString: '00:38'
+    timeString: '00:56'
   },
   {
     avatarSrc: '../images/avatars/AlexW.jpg',
     name: 'Alex Wilber',
     comment: 'Thanks Satya for stepping up and have a great human perspective on this manner.',
-    timeString: '00:40'
+    timeString: '01:05'
   },
   {
     avatarSrc: '../images/avatars/GradyA.jpg',
     name: 'Grady Archie',
     comment: 'Diversity is key to innovation.',
-    timeString: '00:48'
+    timeString: '01:14'
   },
   {
     avatarSrc: '../images/avatars/PattiF.jpg',
     name: 'Patti Fernandez',
     comment: 'It is been great to see how quickly Microsoft has moved to identify the affected employees, put out an official statement and engage in dialogue with the administration about recent events.',
-    timeString: '00:53'
+    timeString: '01:20'
   },
   {
     avatarSrc: '../images/avatars/LeeG.jpg',
     name: 'Lee Gu',
     comment: 'Thank you to the employees at Microsoft for their statement and questions.',
-    timeString: '00:59'
+    timeString: '01:25'
   },
 ];
